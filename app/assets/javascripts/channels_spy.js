a41b1e03fb44601cb11a358b6843303ed8934ed3
Heorhiy Kharvat <zaynyatyi@gmail.com>
2015-09-13 16:11:40 +0300

    Indentation fixed

--- app/assets/javascripts/channels_spy.js
+++ app/assets/javascripts/channels_spy.js
@@ -1,64 +1,64 @@
 $(document).ready(function() {
-function unbindAll() {
-  stalkers.unbind("stalkers_list_changed");
-}
-
-function initDispatcher() {
-  dispatcher = new WebSocketRails(window.location.hostname + ':3001/websocket');
-
-  dispatcher.bind('connection_closed', function() {
-    console.log("client was disconnected");
-    $("#whoopsModal").modal({ backdrop: 'static', keyboard: false });
-  });
-
-  // subscribe to the bind dispatcher channel
-  binder = dispatcher.subscribe('binder');
-
-  stalkers = dispatcher.subscribe('stalkers');
-
-  // bind to a channel event
-  binder.bind('rebind', handlePlaceChanged);
-
-  var placeHolder = $("#place_holder");
-  var currentPlace = placeHolder.data("place");
-  handlePlaceChanged({ new_room: placeHolder.data("place") });
-}
-
-function handlePlaceChanged(data) {
-  console.log('rebinding initiated: ' + data);
-  unbindAll();
-  switch (data.new_room) {
-    case "tower":
-      bindTowerListeners();
-    break;
+  function unbindAll() {
+    stalkers.unbind("stalkers_list_changed");
   }
-}
 
-function bindTowerListeners() {
-  stalkers.bind("stalkers_list_changed", refreshStalkersList);
-}
+  function initDispatcher() {
+    dispatcher = new WebSocketRails(window.location.hostname + ':3001/websocket');
 
-//Channels callbacks
-function refreshStalkersList(data) {
-  console.log("stalkers list changed");
-  $.ajax({
-        url:  "/cities/1/select_users",
-        dataType: "json",
-        data: {},
-        success: function(data) {
-           console.log(data);
-           var stalkersList = $("#stalkers_list ul");
-           stalkersList.empty();
-           data.users.forEach(function(data){
-             stalkersList.append("<li>" + data + "</li>");
-           });
-        }
+    dispatcher.bind('connection_closed', function() {
+      console.log("client was disconnected");
+      $("#whoopsModal").modal({ backdrop: 'static', keyboard: false });
     });
-}
 
-var dispatcher;
-var binder;
-var stalkers;
+    // subscribe to the bind dispatcher channel
+    binder = dispatcher.subscribe('binder');
 
-initDispatcher();
+    stalkers = dispatcher.subscribe('stalkers');
+
+    // bind to a channel event
+    binder.bind('rebind', handlePlaceChanged);
+
+    var placeHolder = $("#place_holder");
+    var currentPlace = placeHolder.data("place");
+    handlePlaceChanged({ new_room: placeHolder.data("place") });
+  }
+
+  function handlePlaceChanged(data) {
+    console.log('rebinding initiated: ' + data);
+    unbindAll();
+    switch (data.new_room) {
+      case "tower":
+        bindTowerListeners();
+      break;
+    }
+  }
+
+  function bindTowerListeners() {
+    stalkers.bind("stalkers_list_changed", refreshStalkersList);
+  }
+
+  //Channels callbacks
+  function refreshStalkersList(data) {
+    console.log("stalkers list changed");
+    $.ajax({
+      url:  "/cities/1/select_users",
+      dataType: "json",
+      data: {},
+      success: function(data) {
+        console.log(data);
+        var stalkersList = $("#stalkers_list ul");
+        stalkersList.empty();
+        data.users.forEach(function(data){
+          stalkersList.append("<li>" + data + "</li>");
+        });
+      }
+    });
+  }
+
+  var dispatcher;
+  var binder;
+  var stalkers;
+
+  initDispatcher();
 });
