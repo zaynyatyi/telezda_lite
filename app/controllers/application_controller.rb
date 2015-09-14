ad9fc61e5ba232e89fa1c677de8c9651c2d4ec47
Heorhiy Kharvat <zaynyatyi@gmail.com>
2015-09-14 09:57:12 +0300

    Stalkers cleanup method added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  def clear_stalkers
+    users = User.all
+    for user in users do
+      user.is_stalker = false
+      user.save()
+    end
+  end
 end
